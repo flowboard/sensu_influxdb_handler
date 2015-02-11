d54bbbb33d26d5d29c567e6d6f7303cac473ded8
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2015-02-10 21:13:18 -0800

    Updates handler to pass rubocop.

--- influx.rb
+++ influx.rb
@@ -3,56 +3,82 @@ require 'influxdb'
 require 'timeout'
 require 'json'
 
-module Sensu::Extension
-
-  class Influx < Handler
-
-    def name
-      'influx'
-    end
-
-    def description
-      'outputs metrics to InfluxDB'
-    end
-
-    def post_init
-      @influxdb = InfluxDB::Client.new settings['influx']['database'], :host => settings['influx']['host'], :port => settings['influx']['port'], :username => settings['influx']['user'], :password => settings['influx']['password']
-      @timeout = @settings['influx']['timeout'] || 15
-    end
-
-    def run(event)
-      begin
-        event = JSON.parse(event)
-        host = event['client']['name']
-        series = event['check']['name']
-        timestamp = event['check']['issued']
-        duration = event['check']['duration']
-        output = event['check']['output']
-      rescue => e
-        @logger.error("InfluxDB: Error setting up event object - #{e.backtrace.to_s}")
+module Sensu
+  module Extension
+    # Sensu Influx Handler
+    class Influx < Handler
+      def name
+        'influx'
       end
 
-      begin
-        points = []
-        output.split(/\n/).each do |line|
+      def description
+        'outputs metrics to InfluxDB'
+      end
+
+      def post_init
+        @influxdb = InfluxDB::Client.new(
+          settings['influx']['database'],
+          host: settings['influx']['host'],
+          port: settings['influx']['port'],
+          username: settings['influx']['user'],
+          password: settings['influx']['password']
+        )
+
+        @timeout = @settings['influx']['timeout'] || 15
+      end
+
+      def parse_event(event)
+        event = JSON.parse(event)
+        [
+          event['client']['name'],
+          event['check']['name'],
+          event['check']['output']
+        ]
+      rescue => e
+        @logger.error "InfluxDB: Error setting up event object - #{e.backtrace}"
+      end
+
+      def strip_metric(metric)
+        return metric unless @settings['influx']['strip_metric']
+        metric.gsub(/^.*#{@settings['influx']['strip_metric']}\.(.*$)/, '\1')
+      end
+
+      def parse_value(value)
+        return Integer(value) if value.match('\.').nil?
+        return Float(value)
+      rescue
+        value.to_s
+      end
+
+      def parse_output_lines(output, host)
+        output.split(/\n/).map do |line|
           @logger.debug("Parsing line: #{line}")
-	  k,v,t = line.split(/\s+/)
-          v = v.match('\.').nil? ? Integer(v) : Float(v) rescue v.to_s
-          k.gsub!(/^.*#{@settings['influx']['strip_metric']}\.(.*$)/, '\1') if @settings['influx']['strip_metric']
-          points << {:time => t.to_f, :host => host, :metric => k, :value => v}
+          m, v, t = line.split(/\s+/)
+
+          m = strip_metric(m)
+
+          v = parse_value(v)
+
+          points << { time: t.to_f, host: host, metric: m, value: v }
         end
       rescue => e
-        @logger.error("InfluxDB: Error parsing output lines - #{e.backtrace.to_s}")
-        @logger.error("InfluxDB: #{output}")
+        @logger.error "InfluxDB: Error parsing output lines - #{e.backtrace}"
+        @logger.error "InfluxDB: #{output}"
       end
 
-      begin
-        @influxdb.write_point(series, points, true)
-      rescue => e
-        @logger.error("InfluxDB: Error posting event - #{e.backtrace.to_s}")
+      def run(event)
+        host, series, output = parse_event(event)
+
+        points = parse_output_lines(output, host)
+
+        begin
+          @influxdb.write_point(series, points, true)
+        rescue => e
+          @logger.error("InfluxDB: Error posting event - #{e.backtrace}")
+        end
+
+        yield('InfluxDB: Handler finished', 0)
       end
-      yield("InfluxDB: Handler finished", 0)
     end
-
   end
 end
