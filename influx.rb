d2d768095535b2fea7cf2a5eb1f0f1ff63ec643a
Daniel Paul Searles <daniel.paul.searles@gmail.com>
2015-02-11 09:01:34 -0800

    Moves metric and host name to be a part of series name.
    This improves performance due to fields not being indexed. Series names
    are indexed so that looking up the series data in this form works a
    whole lot faster.
    
    There was more refactoring that needed to be done in order to pass
    rubocop with these changes.

--- influx.rb
+++ influx.rb
@@ -30,8 +30,7 @@ module Sensu
       def parse_event(event)
         event = JSON.parse(event)
         [
-          event['client']['name'],
-          event['check']['name'],
+          "#{event['check']['name']}.#{event['client']['name']}",
           event['check']['output']
         ]
       rescue => e
@@ -50,29 +49,39 @@ module Sensu
         value.to_s
       end
 
-      def parse_output_lines(output, host)
-        output.split(/\n/).map do |line|
+      def split_line(line)
+        m, v, t = line.split(/\s+/)
+        [strip_metric(m), parse_value(v), t]
+      end
+
+      def add_point(points, metric, point)
+        points[metric] = [] unless points[metric]
+        points[metric] << point
+        points
+      end
+
+      def parse_output_lines(output)
+        points = []
+        output.split(/\n/).each do |line|
           @logger.debug("Parsing line: #{line}")
-          m, v, t = line.split(/\s+/)
-
-          m = strip_metric(m)
-
-          v = parse_value(v)
-
-          points << { time: t.to_f, host: host, metric: m, value: v }
+          m, v, t = split_line(line)
+          points = add_point(points, m, time: t.to_f, value: v)
         end
+        points
       rescue => e
         @logger.error "InfluxDB: Error parsing output lines - #{e.backtrace}"
         @logger.error "InfluxDB: #{output}"
       end
 
       def run(event)
-        host, series, output = parse_event(event)
+        series, output = parse_event(event)
 
-        points = parse_output_lines(output, host)
+        points = parse_output_lines(output)
 
         begin
-          @influxdb.write_point(series, points, true)
+          points.each do |metric, metric_points|
+            @influxdb.write_point("#{series}.#{metric}", metric_points, true)
+          end
         rescue => e
           @logger.error("InfluxDB: Error posting event - #{e.backtrace}")
         end
